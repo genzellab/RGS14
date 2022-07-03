47136abd37f094729df7337026d717d1794cf544
Pelin Özsezer <pelinozsezer@gmail.com>
2022-07-03 16:40:16 +0300

    Delete Pelin directory

--- Pelin/extract_condition.m
+++
@@ -1,7 +0,0 @@
-%% CHANGE VARIABLE NAMES MANUALLY!
-load('waveforms_cluster1_veh.mat');
-data  = waveforms_cluster1_bp_veh;
-HC_i = contains(data(:,8),'HC');
-OS_i = ~contains(data(:,8),'HC');
-waveforms_cluster1_bp_veh_HC = data(HC_i,:);
-waveforms_cluster1_bp_veh_OS = data(OS_i,:);
